2eaf1232cc0f96681431831d3f4fbb38c4fb84a3
Anders Balleby <andersballeby42@gmail.com>
2026-02-28 16:30:41 +0100

    Tilføjet loop  generator med tests

--- src/components/BlocklyWorkspace.jsx
+++ src/components/BlocklyWorkspace.jsx
@@ -4,6 +4,7 @@ import "../blocks/text/text_generator";
 import "../blocks/conditionals/conditional_generator";
 import "../blocks/functions/function_generators";
 import "../blocks/lists/list_generator";
+import "../blocks/loops/loop_generator";
 import { useEffect, useRef } from "react";
 import * as Blockly from "blockly";
 import { javascriptGenerator } from "blockly/javascript";
